2b18588a151d273930105019a2c8c3a9d0669c1c
sudo200 <65343982+sudo200@users.noreply.github.com>
2023-03-25 08:24:48 +0100

    Set UID and GID to 0 (root) Add comments

--- src/main.c
+++ src/main.c
@@ -2,18 +2,25 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include <grp.h>
+
 int main(int argc, char **argv)
 {
-  setlocale(LC_ALL, "");
+  setlocale(LC_ALL, ""); // Show help
   if(argc < 2) {
     printf("Usage: %s <program> [args]\n", *argv);
     return 1;
   }
 
-  argv++;
+  setuid(0);// Be root
+  setgid(0);
+
+  setgroups(0, NULL); // Drop supplementary groups
+
+  argv++; // Execute process
   execvp(*argv, argv);
 
-  perror(*argv);
+  perror(*argv); // Show error
   return 1;
 }
 
